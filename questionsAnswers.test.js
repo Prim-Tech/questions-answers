5d199e1f2c8179cbf230d915718b42c8a6ca0942
Stanley Chu <chustanleys@gmail.com>
2023-05-04 23:35:06 -0700

    added server test

--- questionsAnswers.test.js
+++ questionsAnswers.test.js
@@ -5,17 +5,35 @@ test("add 1 + 2 to equal 3", () => {
   expect(1 + 2).toBe(3);
 });
 
-describe("GET REQUEST TO SERVER TESTING EXPRESS ROUTES", () => {
-  it("responds with text/html and status code 200", (done) => {
-    console.log(done);
-    request(app)
-      .get("/test")
-      .expect("Content-Type", "text/html; charset=utf-8")
-      .expect("Content-Length", "9")
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.text).toBe("test pass");
-        done();
+describe("Test the endpoint response", () => {
+  test("It should respond with a 200 status code", () => {
+    return request(app)
+      .get("/qa/questions?product_id=3") // Replace with your endpoint URL
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
       });
   });
 });
+
+describe("Testing the /qa/questions endpoint with product_id = 3 to see if it returns an object", () => {
+  test("It should respond with a 200 status code", () => {
+    return request(app)
+      .get("/qa/questions?product_id=3") // Replace with your endpoint URL
+      .then((response) => {
+        console.log(JSON.parse(response.text));
+        expect(JSON.parse(response.text)).toBeInstanceOf(Object);
+      });
+  });
+});
+
+// describe("Testing the /qa/questions endpoint with product_id = 3 to see if it returns an object", () => {
+//   test("It should respond with a 200 status code", () => {
+//     return request(app)
+//       .get("/qa/questions?product_id=3") // Replace with your endpoint URL
+//       .then((response) => {
+//         expect(JSON.parse(response.text)[0].results[0].answers).toBeInstanceOf(
+//           Array,
+//         );
+//       });
+//   });
+// });
