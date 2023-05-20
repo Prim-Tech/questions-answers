e8b0340414fc6d94c7203b937e2fce35b570547c
Stanley Chu <chustanleys@gmail.com>
2023-05-20 08:07:28 -0700

    small changes right before deployment

--- PostgreSQL.js
+++ PostgreSQL.js
@@ -1,16 +1,18 @@
 const { Client } = require("pg");
+require("dotenv").config();
 
 const client = new Client({
-  user: "stanleychu",
-  host: "localhost",
-  database: "sdcqa",
-  password: "",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 client.connect((err) => {
   if (err) {
     console.log("ERROR CONNECTING TO POSTGRESQL SERVER");
+    console.log(process.env.DB_USER);
   } else {
     console.log("SUCESSFULLY CONNECTED TO POSTGRESQL SERVER");
   }
